ed85c2553ff234c338199622940eafd6fa7466c3
Lemar Gray <programmerlemar@gmail.com>
2019-01-27 10:43:51 -0500

    Enable pretty url

--- config/web.php
+++ config/web.php
@@ -43,14 +43,15 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
